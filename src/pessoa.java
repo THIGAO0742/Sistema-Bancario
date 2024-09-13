f6da482e572b086c6732a91d0cfb16c98fbc6cb0
TonCerques <zcoltyy@gmail.com>
2024-09-13 17:02:56 -0300

    feat: Dados Pessoais

--- src/pessoa.java
+++ src/pessoa.java
@@ -1,5 +1,27 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public abstract class pessoa {
+
+        protected String nome;
+        protected String cpf;
+        protected int idade;
+        
+        public String getNome() {
+            return nome;
+        }
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+        public String getCpf() {
+            return cpf;
+        }
+        public void setCpf(String cpf) {
+            this.cpf = cpf;
+        }
+        public int getIdade() {
+            return idade;
+        }
+        public void setIdade(int idade) {
+            this.idade = idade;
+        }
+
     }
-}
+
